ec9737cd96fa003eaaa45426901bd508b71a1736
Rabbid <busnka777@gmail.com>
2022-05-16 17:00:20 +0300

    add save image button

---
+++ src/canvas.js
@@ -0,0 +1,3 @@
+// html2canvas(document.querySelector(".container")).then(canvas => {
+//     document.body.appendChild(canvas);
+// });
--- src/index.js
+++ src/index.js
@@ -31,10 +31,10 @@ function clearContainer() {
 }
 
 function drawGrid(numberOfColumn) {
-    let container = document.querySelector(`.container`);
+    let drawingArea = document.querySelector(`#drawing-area`);
     let grid = document.createElement(`div`);
     grid.classList.add(`grid`);
-    container.appendChild(grid);
+    drawingArea.appendChild(grid);
 
     let numberOfRows = Math.round(numberOfColumn * (2 / 3));
     grid.setAttribute(`style`, `grid-template-columns: repeat(${numberOfColumn}, 1fr); 
@@ -47,6 +47,7 @@ function drawGrid(numberOfColumn) {
     }
 
     createOutputInfo(numberOfColumn,numberOfRows);
+    createSaveImageButton();
 }
 
 function createOutputInfo(numberOfColumn, numberOfRows) {
@@ -54,6 +55,41 @@ function createOutputInfo(numberOfColumn, numberOfRows) {
     span.classList.add(`output`);
     span.textContent=`Количество клеток: ${numberOfColumn*numberOfRows}  (${numberOfColumn}x ${numberOfRows}y)`;
     document.querySelector(`.header`).appendChild(span);
+}
+
+function createSaveImageButton(){
+    let button = document.createElement(`button`);
+    button.classList.add(`save-button`);
+    button.textContent = `Сохранить изображение`;
+    button.addEventListener(`click`, saveImage);
+    document.querySelector(`.header`).appendChild(button);
+}
+
+function saveImage(e){
+    html2canvas(document.querySelector("#drawing-area")).then(canvas => {
+        let container = document.querySelector(`#canvas-container`);
+        container.classList.toggle(`hide`);
+        canvas.style.top = `${Math.floor((document.documentElement.clientHeight - canvas.height) / 2)}px`;
+        canvas.style.left = `${Math.floor((document.documentElement.clientWidth - canvas.width) / 2) + 1}px`;
+        createExitButton();
+        container.appendChild(canvas);
+    })
+}
+
+function createExitButton(){
+    let button = document.createElement(`div`);
+    button.classList.add(`exit-button`);
+    button.innerHTML = `<img src = img/x-mark.png alt = 'exit'>`;
+    button.addEventListener(`click`, returnToDraw);
+    document.querySelector(`#canvas-container`).appendChild(button);
+}
+
+function returnToDraw(){
+    let container = document.querySelector(`#canvas-container`);
+    container.classList.toggle(`hide`);
+    for (let child of container.childNodes){
+        container.removeChild(child);
+    }
 }
 
 function paintCell(e) {
